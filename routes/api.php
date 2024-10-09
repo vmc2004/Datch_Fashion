c53c2ddb365208192a2e69a62c5712dc5a215d7c
update <thainhph44403@fpt.edu.vn>
2024-10-09 14:48:01 +0700

    Api products

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
 
 
 /*
@@ -19,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::apiResource('products', ProductController::class);
+Route::apiResource('products', ProductController::class);
