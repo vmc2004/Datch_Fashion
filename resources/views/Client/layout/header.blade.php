9dfcbc3c05f51814015928903cf2c838df15b189
Vũ Minh Chiến <162954413+vmc2004@users.noreply.github.com>
2024-10-28 12:39:02 +0700

    Chi tiết sản phẩm

--- resources/views/Client/layout/header.blade.php
+++ resources/views/Client/layout/header.blade.php
@@ -4,12 +4,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="{{asset('assets/admin/img/logDatch.png')}}" type="image/x-icon">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" href="{{asset('assets/clinet/css/styles.css')}}">
   <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
   <title>@yield('title') - Datch</title>
-  <link rel="icon" type="image/x-icon" href="/favicon.png">
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" href="{{asset('assets/client//assets/css/styles.css')}}">
   <link rel="stylesheet" href="{{asset('assets/client/assets/css/styles-be.css')}}">
@@ -24,7 +24,9 @@
           <nav class="flex items-center justify-between">
             <div class="flex items-center space-x-8">
                 <div class="">
+                   <a href="/">
                     <img src="{{asset('assets/admin/img/Datch.png')}}" alt="" width="150px" >
+                   </a>
                 </div>
                 <a href="#" class="text-gray-800 font-semibold">SALE</a>
                 <a href="#" class="text-gray-800 font-semibold">SẢN PHẨM MỚI</a>
@@ -38,7 +40,7 @@
                     <input type="text" placeholder="Tìm kiếm" class="pl-10 pr-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-gray-300">
                     <i class="fas fa-search absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"></i>
                 </div>
-                <a href="/cua-hang" class="flex flex-col items-center text-gray-800">
+                <a href="/cua-hang" class="flex flex-col items-center text-gray-800 mt-2">
                   <i class="fa-solid fa-shop"></i>
                     <span class="text-sm">Cửa hàng</span>
                 </a>
