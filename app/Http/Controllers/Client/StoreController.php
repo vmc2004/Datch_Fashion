5dd5717b304d88ee50091ddbd536a5b6abee3462
minhvu981 <134676311+minhvu981@users.noreply.github.com>
2024-11-08 10:47:06 +0700

    StoreController

--- app/Http/Controllers/Client/StoreController.php
+++ app/Http/Controllers/Client/StoreController.php
@@ -21,28 +21,4 @@ class StoreController extends Controller
         return view('Client.category.index', compact('category','size','color','products'))->with('i' , (request()->input('page',1)-1)*6);
     }
 
-    public function show($slug)
-    {
-        $product = Product::where('slug', $slug)
-            ->with(['ProductVariants.size', 'ProductVariants.color']) // Nạp các quan hệ
-            ->firstOrFail(); // Đảm bảo bạn nhận được một instance
-        $colors = $product->ProductVariants->unique('color_id')->pluck('color');
-        $sizes = $product->ProductVariants->unique('size_id')->pluck('size');
-        // $product->increment('views');
-        $related_products = Product::where('category_id', $product->category_id)
-            ->where('id', '!=', $product->id)
-            ->take(4) // Giới hạn số lượng sản phẩm liên quan (tùy chỉnh theo ý muốn)
-            ->get();
-
-        $comments = Comment::query()->where('product_id',$product->id)->paginate('9');
-        $avgRating = Comment::query()->where('product_id',$product->id)->avg('rating');
-        return view('Client.product.show', [
-            'product' => $product,
-            'colors' => $colors,
-            'sizes' => $sizes,
-            'related_products' => $related_products,
-            'comments' => $comments,
-            'avgRating' => $avgRating,
-        ]);
-}
 }
